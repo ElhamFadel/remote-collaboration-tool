c4ddd7a20376d824af2b0c6cf09ec199f7697f11
ElhamFadel <elham2000fadel@gmail.com>
2022-06-04 13:58:00 +0300

    Added

--- client/src/components/SpaceNotes/WhiteBoard.js
+++ client/src/components/SpaceNotes/WhiteBoard.js
@@ -1,15 +1,21 @@
 import { useEffect } from 'react';
 import { CardNote } from '../ui';
+import Mitt from 'mitt';
 import { io } from 'socket.io-client';
+const socket = io('http://localhost:5000');
 import * as S from './style';
+const emitter = new Mitt();
+const createNewNote = async () => {
+  await socket.emit('new-note');
+};
 const WhiteBoard = () => {
   useEffect(() => {
-    const socket = io('http://localhost:5000');
     socket.on('connect', () => console.log(socket.id));
+    socket.on('new-note', (noteId, operations) => {});
     socket.on('connect_error', () => {
       setTimeout(() => socket.connect(), 5000);
     });
-    socket.on('disconnect', () => console.log('Hello'));
+    socket.on('disconnect', () => console.log('Client disconnected'));
   }, []);
   return (
     <S.ContainerWhiteBoard>
